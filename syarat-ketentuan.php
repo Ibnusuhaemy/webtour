024df663a038d8a76bb6c493a22d0ce390a01f80
fareladzin123 <fareladzinwibiramadhan@gmail.com>
2019-07-15 10:27:41 +0700

    fixContent

--- syarat-ketentuan.php
+++ syarat-ketentuan.php
@@ -100,77 +100,80 @@
             </div>
             <div class="sk-desc-detail">
               <h1 class="header-subTitle">Pendaftaran & Pembayaran</h1>
-              <p>
+              <ol>
+              <li>
               Uang muka pendaftaran yang dibayarkan kepada JAGAD TOUR tidak dapat dikembalikan (down payment non-refundable) dengan pembayaran down payment sebesar 50% dari total biaya tour secara keseluruhan sesuai dengan e-invoice yang telah diterbitkan
-              </p>
-              <p>
+              </li>
+              <li>
               Peserta diharuskan membayarkan down payment sebelum tanggal jatuh tempo sesuai yang tertera di e-invoice
-              </p>
-              <p>
+              </li>
+              <li>
               Pendaftaran tanpa disertai deposit bersifat tidak mengikat dan dapat dibatalkan tanpa pemberitahuan terlebih dahulu kepada Peserta
-              </p>
-              <p>
+              </li>
+              <li>
               Pelunasan biaya tour dilakukan 2 hari sebelum tanggal keberangkatan setelah peserta mendapatkan kontak tim yang akan menjemput
-              </p>
-              <p>
+              </li>
+              <li>
               Pendaftaran yang dilakukan kurang dari 7 hari sebelum tanggal keberangkatan harus langsung dibayar lunas
-              </p>
-              <p>
+              </li>
+              <li>
               Bagi pendaftar yang berusia di atas 70 tahun atau memiliki keterbatasan fungsi anggota tubuh atau indera atau keterbatasan secara mental, wajib  didampingi oleh anggota keluarga, teman atau saudara yang akan bertanggung jawab selama perjalanan tour
-              </p>
+              </li>
+              </ol>
               <br>
               <h1 class="header-subTitle">Deviasi</h1>
-              <p>
+              <ol>
+              <li>
               Deviasi; Perubahan, perpanjangan, penambahan/penyimpangan rute perjalanan di luar rute perjalanan yang telah dijadwalkan oleh JAGAD TOUR
-              </p>
-              <p>
+              </li>
+              <li>
               Deviasi dapat dilakukan apabila mendapatkan persetujuan dari rombongan dan destinasi yang diinginkan tidak membahayakan dan masih dalam batas kewajaran
-              </p>
-              <p>
+              </li>
+              <li>
               Deviasi dapat diproses apabila sudah melakukan deposit atas besaran biaya penambahan rute perjalanan
-              </p>
-              <p>
+              </li>
+              <li>
               Penambahan harga tiket objek wisata karena pengubahan objek wisata saat berlangsungnya tour, menjadi sepenuhnya tanggung jawab peserta
-              </p>
-              <p>
+              </li>
+              <li>
               Deviasi yang akan mempersingkat jadwal paket tour, tidak diberikan pengurangan biaya dari biaya paket standar semula
-              </p>
+              </li>
+              </ol>
               <br>
               <h1 class="header-subTitle">Pembatalan</h1>
-              <p>
-              Jika terjadi pembatalan acara tour oleh Peserta sebelum tanggal keberangkatan maka biaya pembatalan adalah sebagai berikut:
-              </p>
+              <ol>
+                <li>
+                Jika terjadi pembatalan acara tour oleh Peserta sebelum tanggal keberangkatan maka biaya pembatalan adalah sebagai berikut:
+                </li>
               <ul>
                 <li>Setelah pendaftaran	: Uang muka pendaftaran (Non Refundable)</li>
                 <li>30-15 hari kalender sebelum tanggal keberangkatan       : 25% dari biaya tour</li>
                 <li>14-07 hari kalender sebelum tanggal keberangkatan       : 50% dari biaya tour</li>
                 <li>06-00 hari kalender sebelum tanggal keberangkatan       : 75% dari biaya tour</li>
               </ul>
-              <b>Biaya pembatalan di atas juga berlaku bagi:</b>
-              <br>
-              <p>
-              Peserta yang mengganti tanggal keberangkatan atau mengganti paket/jenis tour
-              </p>
-              <p>
-              Pembatalan diatas tidak berlaku atas kejadian karena bencana alam, perang, wabah penyakit, aksi teroris atau keadaan ‘Force Majeure’ lainnya, maka ketentuan-ketentuan di atas dapat berubah sewaktu-waktu dengan pemberitahuan terlebih dahulu)
-              </p>
-              <p>
-              Force Majeure; Suatu kejadian yang terjadi di luar kemampuan manusia dan tidak dapat dihindarkan sehingga suatu kegiatan tidak dapat dilaksanakan sebagaimana mestinya
-              </p>
-              <p>
-              JAGAD TOUR berhak membatalkan pendaftaran Peserta tour yang belum membayar uang muka atau pelunasan sesuai batas waktu yang telah ditentukan JAGAD TOUR
-              </p>
-              <p>
-              Uang muka pendaftaran Peserta tidak dapat dikembalikan bila Peserta melakukan pembatalan secara sepihak dan pengembalian deposit akan dilakukan maksimal 7 hari kerja setelah terjadi kesepakatan keduanya
-              </p>
+              <li>
+                <b>Biaya pembatalan di atas juga berlaku bagi:</b>
+              </li>
+              <ul>
+                <li>Peserta yang mengganti tanggal keberangkatan atau mengganti paket/jenis tour</li>
+                <li>Pembatalan diatas tidak berlaku atas kejadian karena bencana alam, perang, wabah penyakit, aksi teroris atau keadaan ‘Force Majeure’ lainnya, maka ketentuan-ketentuan di atas dapat berubah sewaktu-waktu dengan pemberitahuan terlebih dahulu)</li>
+                <li>Force Majeure; Suatu kejadian yang terjadi di luar kemampuan manusia dan tidak dapat dihindarkan sehingga suatu kegiatan tidak dapat dilaksanakan sebagaimana mestinya</li>
+                <li>JAGAD TOUR berhak membatalkan pendaftaran Peserta tour yang belum membayar uang muka atau pelunasan sesuai batas waktu yang telah ditentukan JAGAD TOUR</li>
+                <li>Uang muka pendaftaran Peserta tidak dapat dikembalikan bila Peserta melakukan pembatalan secara sepihak dan pengembalian deposit akan dilakukan maksimal 7 hari kerja setelah terjadi kesepakatan keduanya</li>
+              </ul>
+              </ol>
               <br>
               <h1 class="header-subTitle">Harga Khusus Anak</h1>
-              <p>Untuk harga yang ditetapkan untuk anak, JAGAD TOUR menerapkan harga seperti berikut :</p>
+              <ol>
+              <li>
+                <p>Untuk harga yang ditetapkan untuk anak, JAGAD TOUR menerapkan harga seperti berikut :</p>
+              </li>
               <ul>
                 <li>	1 – 3 th = Free (Tidak mendapatkan Bed, Makan & Tiket objek wisata kecuali saat di beberapa objek wisata, ybs tingginya telah melebihi 80cm, maka peserta berhak membeli tiket sendiri)</li>
                 <li>	3,5 – 7th = 75% dari harga tour (Tidak mendapatkan Bed, akan tetapi mendapatkan tiket objek wisata dan meals selama perjalanan)</li>
                 <li>	3,5 – 7th = 50% dari harga tour (tidak mendapatkan bed dan tidak mendapatkan meals selama perjalanan)</li>
               </ul>
+              </ol>
             </div>
           </div>
         </div>

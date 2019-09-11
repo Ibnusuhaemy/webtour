0f16d4e6ab121b9c69c975f842df707daeabc1d4
T-Ibnu <ibnuspeedster@gmail.com>
2019-09-11 20:44:35 +0700

    update css versions and make text align to left

--- blog-detail.php
+++ blog-detail.php
@@ -27,7 +27,7 @@
 
   <link rel="stylesheet" type="text/css" href="vendor/angle-vladov-mobile-slider/jquery.mobile-slider.min.css">
 
-  <link rel="stylesheet" type="text/css" href="css/main.css?v=19">
+  <link rel="stylesheet" type="text/css" href="css/main.css?v=20">
 
 </head>
 
--- package.php
+++ package.php
@@ -25,7 +25,7 @@
   <link rel="stylesheet" type="text/css" href="vendor/datetimepicker.4.17.42/css/bootstrap-datetimepicker.min.css">
   <link rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
 
-  <link rel="stylesheet" type="text/css" href="css/main.css?v=19">
+  <link rel="stylesheet" type="text/css" href="css/main.css?v=20">
 
 </head>
 

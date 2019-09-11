587c51b9ab0b46e77af428111fc563872dc1134f
T-Ibnu <ibnuspeedster@gmail.com>
2019-09-11 19:39:06 +0700

    update css versions

--- about.php
+++ about.php
@@ -27,7 +27,7 @@
   <link rel="stylesheet" type="text/css" href="vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css" />
   <link rel="stylesheet" type="text/css" href="vendor/angle-vladov-mobile-slider/jquery.mobile-slider.min.css">
 
-  <link rel="stylesheet" type="text/css" href="css/main.css?v=17">
+  <link rel="stylesheet" type="text/css" href="css/main.css?v=18">
 
 </head>
 

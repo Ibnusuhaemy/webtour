f82e5da4146e10db6b32779fe31e3d202d0a5079
Ibnu Suhaemy <ibnu@clloh.com>
2019-10-22 13:38:22 +0700

    Fixing margin and padding

--- family-trip.php
+++ family-trip.php
@@ -26,7 +26,7 @@
 
   <link rel="stylesheet" type="text/css" href="vendor/angle-vladov-mobile-slider/jquery.mobile-slider.min.css">
 
-  <link rel="stylesheet" type="text/css" href="css/main.css?v=29">
+  <link rel="stylesheet" type="text/css" href="css/main.css?v=30">
 </head>
 
 <body>
